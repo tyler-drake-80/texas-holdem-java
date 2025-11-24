8c32ebb6c5bf171bd5b1eac783fecb58b7cf9e52
Tyler <tylerdraketd@gmail.com>
2025-11-24 16:11:37 -0500

    Added isFaceUp attribute to Card

--- src/cards/Card.java
+++ src/cards/Card.java
@@ -3,6 +3,7 @@ package cards;
 public class Card{
     private final SUIT suit;
     private final VALUE val;
+    private boolean isFaceUp = false;
 
     public Card(SUIT s, VALUE v){
         this.suit = s;
@@ -12,6 +13,13 @@ public class Card{
     public VALUE getValue(){ return this.val; }
 
     public SUIT getSuit(){ return this.suit; }
+
+    public boolean isFaceUp(){ return this.isFaceUp; }
+    
+    public boolean flip(){
+        this.isFaceUp = !this.isFaceUp;
+        return this.isFaceUp;
+    }
 
     @Override
     public String toString(){ return this.val + " of " + this.suit; }
