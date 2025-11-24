3cfde45128070ed3ed8d6c684c137a1c398caa49
Tyler <tylerdraketd@gmail.com>
2025-11-24 17:14:57 -0500

    Added basic game flow, flop, preFlop, turn, river

--- src/game/GameEngine.java
+++ src/game/GameEngine.java
@@ -1,13 +1,10 @@
 package game;
 
+import cards.Card;
 import cards.Deck;
 import players.Player;
 import java.util.*;
-/*
-    Each player dealt two cards (face down)
-    Then three 'community cards' flopped (middle of table)
-    First betting round ensues, big blind first
-*/
+
 public class GameEngine{
     private final Table table;
     private final Deck deck;
@@ -30,6 +27,28 @@ public class GameEngine{
             }
         }
     }
+    //deal 3 community cards
+    public void flop(){
+        deck.deal();//burn card
+        for(int i = 0; i < 3; i++){//deal three community cards
+            Card c = deck.deal();
+            c.flip();//face up
+            table.addCommunityCard(c);
+        }
+    }
+    //deal 1 community card
+    public void turn(){
+        deck.deal();//burn card
+        Card c = deck.deal();
+        c.flip();//face up
+        table.addCommunityCard(c);
+    }
+    //easier understanding of game function/flow
+    public void river(){ turn();}
+
+    public void bet(){
+        //implement
+    }
     public void start(){
         System.out.println("Welcome to Texas Hold'em!");
 
@@ -42,5 +61,25 @@ public class GameEngine{
         System.out.println("Starting game");
         //further game logic to be implemented
         preFlop();
+        bet();
+        flop();
+        bet();
+        turn();
+        bet();
+        river();
+        //calculate hands, find winner (break out into another function/class?)
+        //use map to associate player to hand weight??
+        List<HAND_WEIGHT> weights = new ArrayList<>();
+        for(Player p : table.getPlayers()){
+            if(!p.isFolded()){
+                System.out.println(p.getName() + "'s hand:");
+                for(Card c : p.getHand()){
+                    System.out.println(c);
+                }
+                HAND_WEIGHT weight = handChecker.checkHand(p, table.getCommunityCards());
+                weights.add(weight);
+            }
+        }
     }    
+    
 }
